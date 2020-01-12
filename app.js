a916884040d4725717e2c1ca19d69da8523d5377
Bruno DaSilva <bananaatomica@Brunos-MacBook-Air.local>
2020-01-11 21:02:52 -0600

    question from 1-6

--- app.js
+++ app.js
@@ -64,22 +64,72 @@ const people = [
   "Blake, William"
 ];
 
+////////////////////////////////
 // Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
+//////////////////////////////
 
+// 1. Filter the list of inventors for those who were born in the 1500's
+const fifteen = inventors.filter(
+  inventor => inventor.year >= 1500 && inventor.year < 1600
+);
+console.table(fifteen);
+
+//////////////////////////////
 // Array.prototype.map()
+//////////////////////////////
+
 // 2. Give us an array of the inventors' first and last names
 
-// Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
+const fullNames = inventors.map(
+  inventor => `First Name is:  ${inventor.first}  Last Name is:${inventor.last}`
+);
+console.table(fullNames);
 
+//////////////////////////////
+// Array.prototype.sort()
+//////////////////////////////
+
+// 3. Sort the inventors by birthdate, oldest to youngest
+// const birthByOrder = inventors.sort(function(firstPerson, secondPerson) {
+//   if (firstPerson.year > secondPerson.year) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+const birthByOrder = inventors.sort((a, b) => (a.year > b.year ? 1 : -1));
+
+console.table(birthByOrder);
+
+//////////////////////////////
 // Array.prototype.reduce()
+//////////////////////////////
+
 // 4. How many years did all the inventors live all together?
+const yearTotal = inventors.reduce((total, inventor) => {
+  return total + (inventor.passed - inventor.year);
+}, 0);
+
+console.log(yearTotal);
 
 // 5. Sort the inventors by years lived
+const sortByOldest = inventors.sort(function(a, b) {
+  const lastGuy = a.passed - a.year;
+  const nextGuy = b.passed - b.year;
+  return lastGuy > nextGuy ? -1 : 1;
+});
+
+console.table(sortByOldest);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+
+// const category = document.querySelector('.mw-category');
+// const links = Array.from(category.querySelectorAll('a'));
+// const de = links
+//             .map(link => link.textContent)
+//             .filter(streetName => streetName.includes('de'));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
